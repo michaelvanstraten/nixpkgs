fe096e344339d38e52c71c73f9d4f2a242b97ec3
Michael van Straten <michael@vanstraten.de>
2026-05-10 23:11:59 +0200

    nixos(services.olm): Init

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1338,6 +1338,7 @@
   ./services/networking/ofono.nix
   ./services/networking/oidentd.nix
   ./services/networking/oink.nix
+  ./services/networking/olm.nix
   ./services/networking/onedrive.nix
   ./services/networking/openconnect.nix
   ./services/networking/openvpn.nix
